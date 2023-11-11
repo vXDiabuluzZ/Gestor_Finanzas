f4d00e257bb86694259bb53f7298b49899c1c721
Miguel Anjel Salazar Jaramillo <miguesalas2004@gmail.com>
2023-11-11 18:32:39 -0500

    el lsitview de los consejos y items_consejos funciona

---
+++ app/src/main/java/com/miguel/getor_finanzas/items_consejos.java
@@ -0,0 +1,14 @@
+package com.miguel.getor_finanzas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class items_consejos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_items_consejos);
+    }
+}
