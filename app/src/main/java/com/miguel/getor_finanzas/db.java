70df057daeb3ce110d339c167bf7c7a5f6d429c6
Miguel Anjel Salazar Jaramillo <miguesalas2004@gmail.com>
2023-11-18 19:12:39 -0500

    la estructura de la base de datos para los consejos está lista

---
+++ app/src/main/java/com/miguel/getor_finanzas/db.java
@@ -0,0 +1,37 @@
+package com.miguel.getor_finanzas;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class db extends SQLiteOpenHelper {
+
+    private static final String name_db = "Gestor_db";
+    private static final int version_db=1;
+    private static final String TABLE_CONSEJOS="CREATE TABLE CONSEJOS(id INT  PRIMERY KEY AUTOINCREMENT, titulo TEXT, descripcion TEXT)";
+
+    public db(@Nullable Context context) {
+        super(context, name_db, null, version_db);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CONSEJOS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONSEJOS);
+        db.execSQL(TABLE_CONSEJOS);
+    }
+
+    public void AgregarConsejos(String titulo_para, String descripcion_para){
+        SQLiteDatabase db2=getWritableDatabase();
+        if(db2!=null){
+            db2.execSQL("INSERT INTO CONSEJOS (titulo, descripcion) VALUES ('"+titulo_para+", "+descripcion_para+" ') ");
+            db2.close();
+        }
+    }
+}
